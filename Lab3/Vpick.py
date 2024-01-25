4f06d3e9ccb2b26b1d643a01ecc6d56a405c5e75
MrMJ <68576550+minjun1@users.noreply.github.com>
2024-01-25 12:13:31 -0800

    Add files via upload

---
+++ Lab3/Vpick.py
@@ -0,0 +1,48 @@
+import Picking
+import Grey
+import Vscan
+import sep_python.sep_vector as SepVector
+from sep_python.hypercube import Hypercube
+import numpy as np
+
+class Vpick:
+
+    def __init__(self,plt,data,vmin=1.5,vmax=4,dv=0.1,**kw):
+        self.panels = []
+        fig, axes = plt.subplots(1,2)
+        self.buttons = Picking.Buttons(axes[1])
+        self.buttons.show()
+
+        vels = self.create_vels(data,vmin,vmax,dv)
+        nmo_data = data.clone()
+        vscan = Vscan.Vscan(vels,data)
+        vscan.adjoint(False,vels,data)
+        vels = self.envelope(vels)
+
+        # original data
+        self.panels.append(Grey.Grey(plt,data,axis=axes[0],title='CMP gather',**kw))
+        # nmo-ed data
+        # self.panels.append(Grey.Grey(plt,nmo_data,axis=axes[1],title='NMO-corrected',**kw))
+        # vscan
+        self.panels.append(Grey.Grey(plt,vels,axis=axes[1],cmap='rainbow',title='Velocity scan',**kw))
+
+    def create_vels(self,data,vmin,vmax,dv):
+        nv = int((vmax-vmin)/dv)
+        vels = SepVector.get_sep_vector(Hypercube.set_with_ns(ns=[data.get_hyper().get_axis(1).n,nv],
+                                                    ds=[data.get_hyper().get_axis(1).d,dv],
+                                                    os=[data.get_hyper().get_axis(1).o,vmin],
+                                                    labels=['','Velocity(km/s)']))
+        return vels
+
+    def envelope(self,data):
+        array = data.getNdArray()
+        fft = np.fft.fft(array,norm='ortho')
+        fft[:,0] = 1
+        fft[:,1:fft.shape[1]//2] *= 2
+        fft[:,fft.shape[1]//2] = 1
+        array[:] = np.abs(np.fft.ifft(fft,norm='ortho'))
+        return data
+
+    def output(self):
+        for panel in self.panels:
+            panel.output()
---
+++ Lab3/Vscan.py
@@ -0,0 +1,89 @@
+import numpy as np
+
+class Vscan:
+
+    """docstring for Vscan."""
+
+    def __init__(self, model, data):
+        ax1 = data.get_hyper().get_axis(1)
+        tmax = ax1.o + (ax1.n - 1) * ax1.d
+        self.tmin = ax1.o
+        self.tmax = ax1.n
+        self.dt = ax1.d
+        ax2 = data.get_hyper().get_axis(2)
+        offmax = ax2.o + (ax2.n - 1) * ax2.d
+        ax3 = model.get_hyper().get_axis(2)
+        vmax = ax3.o + (ax3.n - 1) * ax3.d
+
+        self.t0 = np.linspace(ax1.o,tmax,ax1.n)
+        self.h = np.linspace(ax2.o,offmax,ax2.n)
+        self.v = np.linspace(ax3.o,vmax,ax3.n)
+
+
+    def itime(self,t0,h,v):
+        return int((np.sqrt(t0*t0 + h*h / (v*v)) - self.tmin) / self.dt)
+
+    def forward(self, add, model, data):
+        if (not add): data.scale(0)
+        modNd = model.getNdArray()
+        datNd = data.getNdArray()
+        for iv in range(modNd.shape[0]):
+            for ih in range(datNd.shape[0]):
+                for it in range(modNd.shape[1]):
+                    itt = self.itime(self.t0[it],self.h[ih],self.v[iv])
+                    if itt < self.tmax: datNd[ih,itt] += modNd[iv,it]
+
+    def adjoint(self, add, model, data):
+        if (not add): model.scale(0)
+        modNd = model.getNdArray()
+        datNd = data.getNdArray()
+        for iv in range(modNd.shape[0]):
+            for ih in range(datNd.shape[0]):
+                for it in range(modNd.shape[1]):
+                    itt = self.itime(self.t0[it],self.h[ih],self.v[iv])
+                    if itt < self.tmax: modNd[iv,it] += datNd[ih,itt]
+
+
+class NMO:
+    """docstring for NMO."""
+
+    def __init__(self, model, data, vpick):
+
+        ax1 = data.get_hyper().get_axis(1)
+        tmax = ax1.o + (ax1.n - 1) * ax1.d
+        self.tmin = ax1.o
+        self.tmax = ax1.n
+        self.dt = ax1.d
+        ax2 = data.get_hyper().get_axis(2)
+        offmax = ax2.o + (ax2.n - 1) * ax2.d
+
+        self.t0 = np.linspace(ax1.o,tmax,ax1.n)
+        self.h = np.linspace(ax2.o,offmax,ax2.n)
+
+        self.update_vel(vpick)
+
+    def itime(self,t0,h,v):
+        return int((np.sqrt(t0*t0 + h*h / (v*v)) - self.tmin) / self.dt)
+
+    def update_vel(self,vpick):
+        self.v = np.interp(self.t0,vpick[1],vpick[0],left=vpick[0,0],right=vpick[0,-1])
+
+    # nmo spread
+    def forward(self, add, model, data):
+        if (not add): data.scale(0)
+        modNd = model.getNdArray()
+        datNd = data.getNdArray()
+        for ih in range(datNd.shape[0]):
+            for it in range(datNd.shape[1]):
+                itt = self.itime(self.t0[it],self.h[ih],self.v[it])
+                if itt < self.tmax: datNd[ih,itt] += modNd[ih,it]
+
+    # nmo stack
+    def adjoint(self, add, model, data):
+        if (not add): model.scale(0)
+        modNd = model.getNdArray()
+        datNd = data.getNdArray()
+        for ih in range(datNd.shape[0]):
+            for it in range(datNd.shape[1]):
+                itt = self.itime(self.t0[it],self.h[ih],self.v[it])
+                if itt < self.tmax: modNd[ih,it] += datNd[ih,itt]
